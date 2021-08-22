a98ae748ebf73dd329e0e505bf5d449363c22b59
Jesse Walberg <jesse@jessewalberg.com>
2021-08-22 10:24:20 -0400

    add wrapper width config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    maxWidth: {
+      '9/10': '90%',
+     },
     extend: {
       fontFamily: {
         main: ["SchoolBell", "Helvetica, Arial"],
