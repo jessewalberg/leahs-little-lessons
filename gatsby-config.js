ec589d8edb281711768dcf9da83b59864b3ef252
Jesse Walberg <jesse@jessewalberg.com>
2021-08-30 16:10:14 -0400

    update metadata config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,11 @@ require("dotenv").config({
 const targetAddress = new URL(process.env.TARGET_ADDRESS || `http://leahslittlelessons.com`);
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "leahs-little-lessons",
+    siteUrl: "https://www.leahslittlelessons.com",
+    title: "Leahs Little Lessons",
+    keywords: ["children", "teacher", "parents", "activities", "toddler", "stay at home mom"],
+    author: "Leah Walberg",
+    description: "Leahs little lessons is my space for documenting my experience teaching and raising my child. I add all my activites that are developed based on my years as a preschool teacher."
   },
   plugins: [
     {
@@ -20,11 +23,19 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
   {
     resolve: `gatsby-plugin-canonical-urls`,
     options: {
-        siteUrl: 'leahslittlelessons.com',
+        siteUrl: 'https://leahslittlelessons.com',
     },
-}
+},
+{
+  resolve: `gatsby-plugin-manifest`,
+  options: {
+    start_url: `/`,
+    icon: `src/images/favicon.png`
+  },
+},
   ],
 };
