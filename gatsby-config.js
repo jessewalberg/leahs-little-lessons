f58a24117dca5bf4db7acead70dd8f504282b2c8
Jesse Walberg <jesse@jessewalberg.com>
2021-09-04 08:16:41 -0400

    update sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,12 @@ module.exports = {
     description: "Leahs little lessons is my space for documenting my experience teaching and raising my child. I add all my activites that are developed based on my years as a preschool teacher."
   },
   plugins: [
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: "/",
+      }
+    },
     {
       resolve: `gatsby-source-contentful`,
       options: {
