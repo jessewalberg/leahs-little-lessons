125947880e219236ba407485978345d1b3d19e1c
Jesse Walberg <jesse@jessewalberg.com>
2021-09-04 13:31:48 -0400

    google analytics tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-66T58Q72R8"
+      }
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
