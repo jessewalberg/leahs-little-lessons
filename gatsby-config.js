c2792b53f1c6e767fdf4b789869f04b0a029403d
jessewalberg <jesse@jessewalberg.com>
2023-05-14 15:19:50 -0400

    domain bug fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,26 @@ require("dotenv").config({
 });
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.leahslittlelessons.com",
+    siteUrl: "leahslittlelessons.com",
     title: "Leahs Little Lessons",
-    keywords: ["children", "teacher", "parents", "activities", "toddler", "stay at home mom"],
+    keywords: [
+      "children",
+      "teacher",
+      "parents",
+      "activities",
+      "toddler",
+      "stay at home mom",
+    ],
     author: "Leah Walberg",
-    description: "Leahs little lessons is my space for documenting my experience teaching and raising my child. I add all my activites that are developed based on my years as a preschool teacher."
+    description:
+      "Leahs little lessons is my space for documenting my experience teaching and raising my child. I add all my activites that are developed based on my years as a preschool teacher.",
   },
   plugins: [
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "G-66T58Q72R8"
-      }
+        trackingId: "G-66T58Q72R8",
+      },
     },
     {
       resolve: `gatsby-plugin-google-gtag`,
@@ -22,14 +30,14 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-66T58Q72R8", // Google Analytics / GA
-        ]
-      }
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
         output: "/",
-      }
+      },
     },
     {
       resolve: `gatsby-source-contentful`,
@@ -43,18 +51,18 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
-  {
-    resolve: `gatsby-plugin-canonical-urls`,
-    options: {
-        siteUrl: 'leahslittlelessons.com',
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: "leahslittlelessons.com",
+      },
     },
-},
-{
-  resolve: `gatsby-plugin-manifest`,
-  options: {
-    start_url: `/`,
-    icon: `src/images/favicon.png`
-  },
-},
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        start_url: `/`,
+        icon: `src/images/favicon.png`,
+      },
+    },
   ],
 };
